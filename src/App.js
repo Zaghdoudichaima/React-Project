0b3a679ad0d0609667dc31f039b04c3694f5ce80
Zaghdoudichaima <chaiima.zaghdoudi@gmail.com>
2022-08-27 15:17:39 +0100

    react-project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Form from './components/Form'
+import './App.css';
+import NavBar from './components/NavBar'
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <br/>
+      <Form/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,27 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+
+function BasicExample() {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">Users</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            
+            <NavDropdown title="Action" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Log in</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default BasicExample;
